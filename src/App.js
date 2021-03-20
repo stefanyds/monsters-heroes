5efe622800e88ee8de466a262ff8add6d11cc230
Ap274 <aldopiresaps@gmail.com>
2021-03-20 12:22:38 -0300

    feat: modifying App.js and creating Card-List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getMonsters } from './api';
 import './App.css';
+import CardList from './components/card-list/card-list';
 
 class App extends Component {
   constructor() {
@@ -8,31 +9,27 @@ class App extends Component {
     this.state = {
       monsters: [],
       isError: false,
-      errorMessage: ""
+      errorMessage: '',
     };
   }
 
   render() {
     return (
       <div className="App">
-        {!this.state.isError ?
-          <h1>{this.state.errorMessage}</h1>
-          : ""
-        }
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-          ))}
+        {!this.state.isError ? <h1>{this.state.errorMessage}</h1> : ''}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
-  
-  componentDidMount = () => {
-    getMonsters().then((response) => this.setState({
-      isError: response.ok,
-      errorMessage: response.statusText, 
-      monsters: response.data.monsters 
-    }));
-  }
 
+  componentDidMount = () => {
+    getMonsters().then((response) =>
+      this.setState({
+        isError: response.ok,
+        errorMessage: response.statusText,
+        monsters: response.data.monsters,
+      })
+    );
+  };
 }
 export default App;
