5fac2d25e840acd3ab4ff16d7d3fc87fb65d7c39
Marlo Eichenberg Motta <devmasterjs@programmerhero.com.br>
2021-04-03 12:41:59 -0300

    feat: api rest example (fishes) and unit test

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 export const getMonsters = () => {
-  // const response = fetch('https://jsonplaceholder.typicode.com/users')
-  const response = fetch('http://localhost:3004/users')
+  const response = fetch('https://jsonplaceholder.typicode.com/users')
   .then( async (response) => {
     const responseData = {
       ok: response.ok,
@@ -9,6 +8,20 @@ export const getMonsters = () => {
       data: {
         monsters: response.ok ? await response.json() : []
       }
+    }
+    return responseData;
+  })
+  return response;
+}
+
+export const getPeixes = () => {
+  const response = fetch('http://localhost:3004/peixes')
+  .then( async (response) => {
+    const responseData = {
+      ok: response.ok,
+      status: response.status,
+      statusText: response.ok ? "" : "Lista de monstros indisponível.",
+      data: response.ok ? await response.json() : []
     }
     return responseData;
   })
