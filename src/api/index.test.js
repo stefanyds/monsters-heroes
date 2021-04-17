fac9905bf1848e5bd8a816a7b7f57b30fe08cf33
Marlo Eichenberg Motta <devmasterjs@programmerhero.com.br>
2021-04-17 11:51:07 -0300

    test: updated unit tests with axios

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,15 +1,19 @@
 import { getUsers } from '.';
 
 describe('getUsers', () => {
-  it('should return users list', async () => {
+  it('should return five users in list', async () => {
     const result = await getUsers();
     const list = result.data;
-    // expect(list.length).toBeGreaterThan(0);
-    expect(list).toBe(result.data);
+    expect(list).toHaveLength(5);
   });
   it('should return "luparsene" informations', async () => {
     const result = await getUsers();
-    const list = result.data[4];
-    expect(result.data[4]).toBe(list);
+    const userLuparsene = result.data.find((user) => user.username === "luparsene");
+    expect(userLuparsene.email).toBe("ouilupin@psg.fr");
+    expect(Object.keys(userLuparsene)).toContain("email");
+    expect(Object.keys(userLuparsene)).toContain("password");
+    expect(Object.keys(userLuparsene)).toContain("username");
+    expect(Object.keys(userLuparsene)).toContain("name");
+    expect(Object.keys(userLuparsene)).toContain("id");
   });
 });
