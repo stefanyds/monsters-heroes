916d9056db5441f2437881d86892ac66a16e6023
stefanyds <ste.stefany22@gmail.com>
2021-04-17 13:01:12 -0300

    feat: card styles css

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './card.styles.css';
 
 class Card extends Component {
   render() {
     return (
-      <div>
+      <div className="card-container">
         <img
           src={`https://robohash.org/${this.props.monster.id}?set=set2&size=180x180`}
           alt="Monster"
