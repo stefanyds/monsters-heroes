2f75a6eb17bebec10df5cfe7383dfceff50c36c8
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-05-01 10:49:45 -0300

    test: create mock axios

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,10 +1,40 @@
+import axios from 'axios';
 import { getUsers } from '.';
+
+jest.mock('axios');
+const users = [
+  {
+    id: 1,
+    username: "aldops",
+    name: "Aldo Pires",
+    email: "pires.aldo@disney.com",
+    password: "123oliveira"
+  },
+  {
+    id: 2,
+    username: "stefanyrs",
+    name: "Stefany Souza",
+    email: "souza.stefany@rbr.com",
+    password: "456pao"
+  },
+  {
+    username: "luparsene",
+    name: "Arsene Lupin",
+    email: "ouilupin@psg.fr",
+    password: "04locomotive",
+    id: 5
+  }
+]
+
+beforeEach(() => {
+  axios.get.mockImplementationOnce(() => Promise.resolve({ data: users }))
+});
 
 describe('getUsers', () => {
   it('should return five users in list', async () => {
     const result = await getUsers();
     const list = result.data;
-    expect(list).toHaveLength(5);
+    expect(list).toHaveLength(3);
   });
   it('should return "luparsene" informations', async () => {
     const result = await getUsers();
